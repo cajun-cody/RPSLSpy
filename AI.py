52e73786a502d0a7affa975dbb66accf5bcf722f
Cody <cemt5462@gmail.com>
2022-11-15 17:05:04 -0600

    Tested functionality on game type.

--- AI.py
+++ AI.py
@@ -4,10 +4,16 @@ from player import Player
 class AI(Player):
     def __init__(self):
         super().__init__()
-        self.name = input("What would you like to name the computer? ")
         pass
+
+    def choose_name(self):
+        self.name = input("Enter computer name: ")
+        pass    
 
     def choose_gesture(self):
         self.chosen_gesture = choice(self.gestures)
         print(f'{self.name} throws {self.chosen_gesture}.')
         pass      
+    
+    
+    #self.name = input("What would you like to name the computer? ")
--- game.py
+++ game.py
@@ -12,7 +12,7 @@ class Game:
         self.game_type()
 
     def welcome_greeting(self):
-        print('Welcome to Rock, Paper, Scissors, Lizard, Spock!\n')
+        print('\nWelcome to Rock, Paper, Scissors, Lizard, Spock!')
         print('Game Instructions Are:\n')
         print('Scissors cuts Paper')
         print('Paper covers Rock')
@@ -24,7 +24,7 @@ class Game:
         print('Paper disproves Spock')
         print('Spock vaporizes Rock')
         print('and as it always has,')
-        print('Rock crushes Scissors')
+        print('Rock crushes Scissors\n')
       
     def game_type(self):
         game_type = int(input("Choose game type: (1) Player vs Player or (2) Player vs AI"))
@@ -32,25 +32,30 @@ class Game:
             self.player_two = Human()
             self.player_one.choose_name()
             self.player_two.choose_name()
-            print("Player 1's name is {self.player_one.name}")
-            print("Player 2's name is {self.player_two.name}")
+            print(f"Player 1's name is {self.player_one.name}.")
+            print(f"Player 2's name is {self.player_two.name}.")
         else:
             game_type == 2
             self.player_two = AI()
-            self.player_two.name()
+            self.player_two.choose_name()
             self.player_one.choose_name()
-            print("Player 1's name is {self.player_one.name}")
-            print("Player 2's name is {self.player_two.name}")
-
-
+            print(f"Player 1's name is {self.player_one.name}.")
+            print(f"Player 2's name is {self.player_two.name}.")
+   
     
-    
-    
-    """ def game_round(self):
+    def game_round(self):
         while self.player_one.score < 2 and self.player_two.score < 2:
             print('New Round\n')
+            self.player_one.choose_gesture()
+            self.player_two.choose_gesture()
+            if self.player_one.chosen_gesture == self.player_two.chosen_gesture:
+                print("Round Tied!")
+            elif self.player_one.chosen_gesture == self.player_one.chosen_gesture[0] and self.player_two.chosen_gesture == self.player_two.chosen_gesture[1]:
+                print(f"{self.player_one.name} wins the round.")
+            elif self.player_one.chosen_gesture == self.player_one.chosen_gesture[1] and self.player_two.chosen_gesture == self.player_two.chosen_gesture[2]:
+                print(f"{self.player_one.name} wins the round.")
 
-          """
+          
 
 
    
